954682f73755ce901d95c5052012ff84dcd28967
ggouws <136603000+ggouws@users.noreply.github.com>
2023-07-13 12:28:40 +0100

    Update 03_Reference_mapping.sh

--- 03_Reference_mapping.sh
+++ 03_Reference_mapping.sh
@@ -21,6 +21,6 @@ cd /mnt/fastdata/bi1gg/omnic
 # Create a aligned directory to receive output
 
 # Use bwa mem to map the paired reads to the indexed genome
-bwa mem -5 -P -S -T0 -t20 reference/index 30044_10.asm.bp.p_ctg.fasta \
-raw_data/Oceanic_Cricket_OmniC-S1_L001_R1_001.fastq.gz raw_data/Oceanic_Cricket_OmniC-S1_L001_R2_001.fastq.gz \
+bwa mem -5 -P -S -T0 -t20 reference/30044_10.asm.bp.p_ctg.fasta \
+raw_data/Oceanic-Cricket-OmniC-S1_L001_R1_001.fastq.gz raw_data/Oceanic-Cricket-OmniC-S1_L001_R2_001.fastq.gz \
 -o aligned/Oceanic_cricket_mapped.sam
